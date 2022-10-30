cfc3bcfa149b1fcba8983c7a7eef697ff6806ee2
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2022-10-30 12:45:51 +0000

    minor tweaks

--- value.go
+++ value.go
@@ -18,6 +18,40 @@ func (s S) Len() int      { return 1 }
 func (e E) Len() int      { return 1 }
 func (e E) Error() string { return string(e) }
 
+type AV []V       // generic array
+type AB []bool    // boolean array
+type AF []float64 // real array
+type AI []int     // integer array (TODO: optimization: add Range type?)
+type AS []string  // string array
+
+// Array interface is satisfied by the different kind of supported arrays.
+// Typical implementation is given in comments.
+type Array interface {
+	V
+	At(i int) V           // x[i]
+	Slice(i, j int) Array // x[i:j]
+}
+
+func (x AV) Len() int             { return len(x) }
+func (x AV) At(i int) V           { return x[i] }
+func (x AV) Slice(i, j int) Array { return x[i:j] }
+
+func (x AB) Len() int             { return len(x) }
+func (x AB) At(i int) V           { return B(x[i]) }
+func (x AB) Slice(i, j int) Array { return x[i:j] }
+
+func (x AI) Len() int             { return len(x) }
+func (x AI) At(i int) V           { return I(x[i]) }
+func (x AI) Slice(i, j int) Array { return x[i:j] }
+
+func (x AF) Len() int             { return len(x) }
+func (x AF) At(i int) V           { return F(x[i]) }
+func (x AF) Slice(i, j int) Array { return x[i:j] }
+
+func (x AS) Len() int             { return len(x) }
+func (x AS) At(i int) V           { return S(x[i]) }
+func (x AS) Slice(i, j int) Array { return x[i:j] }
+
 // Verb represents built-in 1-symbol operators.
 type Verb int
 
@@ -52,77 +86,3 @@ const (
 	AFold               // /
 	AScan               // \
 )
-
-type AV []V       // generic array
-type AB []bool    // boolean array
-type AF []float64 // real array
-type AI []int     // integer array (TODO: optimization: add Range type)
-type AS []string  // string array
-
-// Array interface is satisfied by the different kind of supported arrays.
-// Typical implementation is given in comments.
-type Array interface {
-	V
-	At(i int) V           // x[i]
-	Slice(i, j int) Array // x[i:j]
-}
-
-func (x AV) At(i int) V {
-	return x[i]
-}
-
-func (x AV) Len() int {
-	return len(x)
-}
-
-func (x AV) Slice(i, j int) Array {
-	return x[i:j]
-}
-
-func (x AB) At(i int) V {
-	return B(x[i])
-}
-
-func (x AB) Len() int {
-	return len(x)
-}
-
-func (x AB) Slice(i, j int) Array {
-	return x[i:j]
-}
-
-func (x AI) At(i int) V {
-	return I(x[i])
-}
-
-func (x AI) Len() int {
-	return len(x)
-}
-
-func (x AI) Slice(i, j int) Array {
-	return x[i:j]
-}
-
-func (x AF) At(i int) V {
-	return F(x[i])
-}
-
-func (x AF) Len() int {
-	return len(x)
-}
-
-func (x AF) Slice(i, j int) Array {
-	return x[i:j]
-}
-
-func (x AS) At(i int) V {
-	return S(x[i])
-}
-
-func (x AS) Len() int {
-	return len(x)
-}
-
-func (x AS) Slice(i, j int) Array {
-	return x[i:j]
-}
