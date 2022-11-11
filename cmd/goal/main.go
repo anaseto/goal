0e37c3f282d45a2d35e12a5184f8ce5e187d0390
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2022-11-11 18:12:53 +0000

    implement non-interactive command line

--- cmd/goal/main.go
+++ cmd/goal/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"goal"
 	//"log"
@@ -8,6 +10,62 @@ import (
 )
 
 func main() {
+	optE := flag.String("e", "", "command")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-e command] [path]\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "See man page goal(1) for details (TODO).")
+	}
+	flag.Parse()
+	args := flag.Args()
+	if *optE == "" && len(args) == 0 {
+		runStdin()
+		return
+	}
+	ctx := goal.NewContext()
+	say := ctx.RegisterVariadic("say", goal.VariadicFun{
+		Func: func(ctx *goal.Context, args []goal.V) goal.V {
+			switch len(args) {
+			case 1:
+				switch v := args[0].(type) {
+				case goal.S:
+					fmt.Println(v)
+					return nil
+				default:
+					fmt.Printf("%v\n", v)
+					return nil
+				}
+			default:
+				return goal.E("say: bad type")
+			}
+		}})
+	ctx.AssignGlobal("say", say)
+	if *optE != "" {
+		runCommand(ctx, *optE)
+	}
+	if len(args) == 0 {
+		return
+	}
+	fname := args[0]
+	v := make(goal.AV, len(args)-1)
+	for _, s := range args[1:] {
+		v = append(v, goal.S(s))
+	}
+	ctx.AssignGlobal("args", v)
+	bs, err := os.ReadFile(fname)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "goal: %v", err)
+		os.Exit(1)
+	}
+	ctx.SetSource(fname, bytes.NewReader(bs))
+	_, err = ctx.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "goal: %v", err)
+		os.Exit(1)
+	}
+}
+
+func runStdin() {
 	ctx := goal.NewContext()
 	ctx.SetSource("-", os.Stdin)
 	for {
@@ -25,8 +83,26 @@ func main() {
 	}
 }
 
+func runCommand(ctx *goal.Context, cmd string) {
+	_, err := ctx.RunString(cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "goal: %v", err)
+		os.Exit(1)
+	}
+}
+
 func echo(v goal.V) {
 	if v != nil {
 		fmt.Printf("%v\n", v)
 	}
+}
+
+func Error(usage bool, msgs ...interface{}) {
+	s := "goal: "
+	s += fmt.Sprint(msgs...)
+	fmt.Fprintln(os.Stderr, s)
+	if usage {
+		flag.Usage()
+	}
+	os.Exit(1)
 }
