af8d55a98d09d1ee1fd0b99ae3554ef0ea550949
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2022-11-20 12:19:35 +0000

    help

--- cmd/goal/main.go
+++ cmd/goal/main.go
@@ -211,7 +211,7 @@ Type help TOPIC or h TOPIC where TOPIC is one of:
 
 Notations:
 	s (string) f (1-arg fun) F (2-args fun)
-	i (integer) N (numeric) x,y (any)
+	i (integer) n (numeric) x,y (any)
 `
 const helpVERBS = `
 VERBS
@@ -252,8 +252,8 @@ x^y without	1 2 3 4^2 3 -> 1 4
 #x  length	#2 4 5 -> 3
 i#y take	2#4 1 5 -> 4 1	    4#3 1 5 -> 3 1 5 3 (cyclic)
 f#y replicate	{0 1 1 0}#4 1 5 3 -> 1 5    {x>0}#2 -3 1 -> 2 1
-_N  floor	_2.3 -> 2
-_s  to lower	_"ABC" -> "abc"
+_N  floor	_2.3 -> 2     _1.5 3.7 -> 1 3
+_S  to lower	_"ABC" -> "abc"     _"AB" "CD" -> "ab" "cd"
 i_x drop	2_3 4 5 6 -> 5 6
 x_y cut		2 5_!10 -> (2 3 4;5 6 7 8 9)
 f_x weed out	{0 1 1 0}_4 1 5 3 -> 4 3    {x>0}_2 -3 1 -> ,-3
